f5c6d2f27b6c429455a676af6699c5a41be20585
Mai Campbell <jirallc@gmail.com>
2019-04-17 02:05:57 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,6 +7,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-    delete artistName.playlist;
+    delete artistName.playlist('Slowdive');
     return artistName;
   }
